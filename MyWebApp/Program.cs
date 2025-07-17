0f0750cb6a42a4c15507f4a6c5e3078ff6d9ca13
cm-karthik <46833831+cm-karthik@users.noreply.github.com>
2025-07-17 16:30:13 +0530

    New changes pushed to the repo.

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World the hot reload is done, so I am chaning this code string to check teh hot reload watch command!");
 
 app.Run();
